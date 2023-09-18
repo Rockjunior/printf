ad97a4c723272e8f7be893b03aa7156b164ba878
Rockjunior <r28081999m@gmail.com>
2023-09-19 00:07:40 +0300

    Moon Knight.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* linking the code
+*/
+
+int main()
+{
+    // Your code here
+    return 0; // This is the exit status of the program
+}
+
+//Auth
+/*
+* Kadimastermind
+* Rockjunior
+*/
